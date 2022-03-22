7b1f54b1c797ccc052a0591f4d4972d762ae639f
sunhwa <2017sunhwa@gmail.com>
2022-03-22 10:05:48 +0900

    ✨ Feat: typescript study assignment

---
+++ packages/naverAssignment/components/Pagination/index.js
@@ -0,0 +1,10 @@
+export default function Pagination() {
+  this.setState = () => {
+    
+  };
+​
+  this.render = () => {
+      return "<div><span>1</span><span>2</span></div>";
+  };
+}
+
---
+++ packages/naverAssignment/script.js
@@ -0,0 +1,3 @@
+import Pagination from './components/Pagination';
+
+const pageing = new Pagination();
