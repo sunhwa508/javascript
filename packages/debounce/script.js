4f816c878e983af3e0775a4980bde13730aa22ac
sunhwa <2017sunhwa@gmail.com>
2022-04-03 10:54:01 +0900

    ✨ Feat: debounce, Throttle

---
+++ packages/debounce/script.js
@@ -0,0 +1,66 @@
+const input = document.querySelector('input');
+const defaultText = document.getElementById('default');
+const debounceText = document.getElementById('debounce');
+const throttleText = document.getElementById('throttle');
+
+// input.addEventListener('input', (e) => {
+//   defaultText.textContent = e.target.value;
+//   updateDebounceText(e.target.value);
+//   updateThrottleText(e.target.value);
+// });
+
+const debounce = (cb, delay = 1000) => {
+  let timeout;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      cb(...args);
+    }, delay);
+  };
+};
+
+const throttle = (cb, delay = 1000) => {
+  let shouldWait = false;
+  let waitingArgs;
+  const timeoutFunc = () => {
+    if (waitingArgs == null) {
+      shouldWait = false;
+    } else {
+      cb(...waitingArgs);
+      waitingArgs = null;
+      setTimeout(timeoutFunc(), delay);
+    }
+  };
+
+  return (...args) => {
+    if (shouldWait) {
+      waitingArgs = args;
+      return;
+    }
+
+    cb(...args);
+    shouldWait = true;
+
+    setTimeout(timeoutFunc, delay);
+  };
+};
+
+const updateDebounceText = debounce((text) => {
+  incrementCount(debounceText);
+  //   debounceText.textContent = text;
+});
+
+const updateThrottleText = throttle((text) => {
+  incrementCount(throttleText);
+  //   throttleText.textContent = text;
+}, 100);
+
+document.addEventListener('mousemove', (e) => {
+  incrementCount(defaultText);
+  updateDebounceText();
+  updateThrottleText();
+});
+
+const incrementCount = (element) => {
+  element.textContent = (parseInt(element.innerText) || 0) + 1;
+};
