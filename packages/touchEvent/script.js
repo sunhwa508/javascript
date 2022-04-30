4180ad7b9b80666134e00b631013ab51106876aa
sunhwa <2017sunhwa@gmail.com>
2022-04-30 17:49:40 +0900

    touch event

---
+++ packages/touchEvent/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('touchstart', (e)=>{
+    console.log('Start')
+})
+document.addEventListener('touchmove', (e)=>{
+    console.log('Move')
+})
+document.addEventListener('touchend', (e)=>{
+    console.log('End')
+})
